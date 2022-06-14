2138c5f1443c1f0030c6a087016ed9bb73eaacaf
George Anya <georgeanya4real@gmail.com>
2022-06-14 14:31:38 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import Homepage from './components/homepage';
-import Navbar from './components/navbar';
-import React from 'react';
-import './App.css';
+import Homepage from "./components/homepage";
+import Navbar from "./components/navbar";
+import React from "react";
+import "./App.css";
 
 function App() {
-  // const [darkMode, setDarkMode] = React.useState('true')
+  const [darkMode, setDarkMode] = React.useState("true");
+
+  function toggleDarkMode() {
+    setDarkMode((prevState) => !prevState);
+  }
   return (
     <div className="App">
-      <Navbar/>
-      <Homepage/>
+      <Navbar toggle={toggleDarkMode} />
+
+      <Homepage />
     </div>
   );
 }
